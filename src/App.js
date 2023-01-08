74f7aad1ccbae104ea18fae6ce6b8efb8e7fb326
MD <mdiyap1996@gmail.com>
2023-01-08 21:07:14 +0100

    Implemented GA

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 import Cards from './Components/Cards.jsx'
 import MainPage from './Components/MainPage';
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = "UA-253439300-1";
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <div>
       <MainPage/>
