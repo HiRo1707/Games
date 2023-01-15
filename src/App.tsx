59d4929a6171d141f2931c9ca2ca84cff39ffac4
MD <mdiyap1996@gmail.com>
2023-01-16 00:22:30 +0100

    added typescript and enabled GA4

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,19 @@ import { useEffect } from 'react';
 import Cards from './Components/Cards.jsx'
 import MainPage from './Components/MainPage';
 import ReactGA from 'react-ga';
+import ReactGA4 from 'react-ga4';
+//import ReactGA from 'react-ga4';
 
-const TRACKING_ID = "UA-253439300-1";
-ReactGA.initialize(TRACKING_ID);
+const TRACKING_ID_GA = "UA-253439300-1";
+const TRACKING_ID_GA4 = "G-CXEN0JB7T2";
+ReactGA.initialize(TRACKING_ID_GA);
+ReactGA4.initialize(TRACKING_ID_GA4);
 
 function App() {
 
   useEffect(() => {
     ReactGA.pageview(window.location.pathname + window.location.search);
+    ReactGA4.event('Homepage');
   }, []);
 
   return (
